b0b895cf04f3372f98be7f9a0fea0ef13635a8d3
edager <edager@users.noreply.github.com>
2024-02-08 14:33:20 +0000

    deploy: b2f760b88357c237a72009a84005004f1e4334ad

--- assets/js/5e95c892.11aa4eeb.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmbdb_docs=self.webpackChunkmbdb_docs||[]).push([[304],{3564:(e,s,c)=>{c.r(s),c.d(s,{default:()=>b});c(1504);var r=c(4064),d=c(5756),u=c(5864),a=c(5464),n=c(9012),t=c(7624);function b(e){return(0,t.jsx)(d.cr,{className:(0,r.c)(u.W.wrapper.docsPages),children:(0,t.jsx)(n.c,{children:(0,a.k)(e.route.routes)})})}}}]);
