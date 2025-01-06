1bf5e35e613c99117ef5f6097716b91b2fc0c1d2
Emil Dandanell Agerschou <emildandanell.agerschou@ibt.cas.cz>
2025-01-06 14:00:22 +0100

    added information about automatic extraction and bumped the version

--- pages/record-workflows/_meta.js
+++ pages/record-workflows/_meta.js
@@ -1,5 +1,6 @@
 export default {
   "intro": "Introduction",
+  "automatic-extraction": "Automatic metadata extraction",
   "prerequisites": "Prerequisites",
   "publication": "Publication",
   "deletion": "Deletion",
